a0e4c05e9e951db7522ce9de2370abcc6602d3e1
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-11-17 22:40:59 +0100

    Update 03_package.py
    variables

--- 03_package.py
+++ 03_package.py
@@ -2,3 +2,11 @@ import os
 from random import setstate
 width, height =os.get_terminal_size()
 width = width - 1
+
+count_packages = 0
+count_kg_packages = 0
+max_weight_package = 20
+min_weight_package = 20
+current_weight_package = 0
+#weight_package = 0
+number_min_weight_package = 0
