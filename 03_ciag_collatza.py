ba4f59d4b00d407411c6bfba0430665d3f75a0a7
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-11-19 18:07:42 +0100

    Update 03_ciag_collatza.py
    add while

--- 03_ciag_collatza.py
+++ 03_ciag_collatza.py
@@ -1,19 +1,25 @@
 count = 0
-x = int(input("Podaj wartosć: "))
-while 1 <= x < 100:
-    while x != 1:
-        if not x %2:
-            x = x/2
-        else:
-            x = 3 * x + 1
-        count +=1
-        print(f"Dla numeracji nr {count}, wartość ciągu - {int(x)} ")
-    check = input("\nCzy chesz kontynouować t - tak,  n - nie: ")
-    if check== "t":
-        x = int(input("Podaj wartosć: "))
-        continue
+print("\nPrawidłowa wartość od 1 do 100")
+x = int(input("\nPodaj wartosć: "))
+while 1 <= x <= 100:
+    if x == 1:
+        print("\nCiąg ma 1 element \n")
+        exit()
     else:
-        break
-print("\nKoniec\n ")
+        while x != 1:
+            if not x % 2:
+                x = x/2
+            else:
+                x = 3 * x + 1
+            count +=1
+ #           print(f"Dla numeracji nr {count}, wartość ciągu wynosi: {int(x)} ")
+        print(f"Ciąg ma {count} elementów")
+        check = input("\n(t)ak -nowa iteracja, (k)oniec ")
+        if check == "t":
+            x = int(input("\nPodaj wartosć: "))
+            count = 0
+            continue
+        exit()
+print("Koniec\n ")
 exit()
 
