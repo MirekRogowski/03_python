56d14b0956be3cc9e2b71ddf02b309a27d314488
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-11-19 12:29:01 +0100

    Create 03_ciag_collatza.py

---
+++ 03_ciag_collatza.py
@@ -0,0 +1,19 @@
+count = 0
+x = int(input("Podaj wartosć: "))
+while True:
+    while x != 1:
+        if not x %2:
+            x = x/2
+        else:
+            x = 3 * x + 1
+        count +=1
+        print(f"Dla numeracji nr {count}, wartość ciągu - {int(x)} ")
+    check = input("\nCzy chesz kontynouować t - tak,  n - nie: ")
+    if check== "t":
+        x = int(input("Podaj wartosć: "))
+        continue
+    else:
+        break
+print("\nKoniec\n ")
+exit()
+
