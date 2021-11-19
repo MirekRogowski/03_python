94448b98746726b7a54834ff13778a197531d426
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-11-19 08:58:46 +0100

    Update 03_kolko.py
    selection of field no.1

--- 03_kolko.py
+++ 03_kolko.py
@@ -1,7 +1,11 @@
 #variables
 
-print("\n")
 while True:
+    print("\n")
+    print("- | - | -       1 | 2 | 3")
+    print("- | X | -       4 | 5 | 6")
+    print("- | - | -       7 | 8 | 9")
+    print("\n")
     pole = int(input("Wybierz numer pola: "))
     if  pole == 1:
         print("\n")
@@ -13,7 +17,117 @@ while True:
         print("O | - | X       1 | 2 | 3")
         print("- | X | -       4 | 5 | 6")
         print("- | - | -       7 | 8 | 9")
-        print("\n")        
+        print("\n")
+        dalej = int(input("Wybierz numer pola: "))
+        if dalej == 2:
+            print("\n")
+            print("O | O | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("- | - | -       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | O | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("X | - | -       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break
+        elif dalej == 4:
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("O | X | -       4 | 5 | 6")
+            print("- | - | -       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("O | X | -       4 | 5 | 6")
+            print("X | - | -       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break            
+        elif dalej == 6:
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | 0       4 | 5 | 6")
+            print("- | - | -       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | 0       4 | 5 | 6")
+            print("X | - | -       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break  
+        elif dalej == 7:
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("0 | - | -       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("X | X | -       4 | 5 | 6")
+            print("0 | - | -       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break  
+        elif dalej == 8:
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("- | 0 | -       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("X | O | -       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break  
+        elif dalej == 9:
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("- | - | 0       7 | 8 | 9")
+            print("\n")
+            print("\n")
+            print("O | - | X       1 | 2 | 3")
+            print("- | X | -       4 | 5 | 6")
+            print("X | - | 0       7 | 8 | 9")
+            print("\n")
+            print("Przegrana")
+            gra = input("Chcesz grać dalej t - tak , n -nie ")
+            if gra == "t":
+                print("Nowa gra")
+                continue
+            else:
+                break  
+    #koniec dla pola numer 1
     elif pole == 2:
         print("\n")
         print("- | O | -       1 | 2 | 3")
@@ -38,8 +152,8 @@ while True:
         print("\n")  
     elif pole == 4:
         print("\n")
-        print("- | - | -       1 |   | 3")
-        print("O | X | -       4 |   | 6")
+        print("- | - | -       1 | 2 | 3")
+        print("O | X | -       4 | 4 | 6")
         print("- | - | -       7 | 8 | 9")
         print("\n")
         print("\n")
@@ -49,8 +163,8 @@ while True:
         print("\n")  
     elif pole == 6:
         print("\n")
-        print("- | - | -       1 |   | 3")
-        print("- | X | 0       4 |   | 6")
+        print("- | - | -       1 | 2 | 3")
+        print("- | X | 0       4 | 4 | 6")
         print("- | - | -       7 | 8 | 9")
         print("\n")
         print("\n")
@@ -60,8 +174,8 @@ while True:
         print("\n")  
     elif pole == 7:
         print("\n")
-        print("- | - | -       1 |   | 3")
-        print("- | X | -       4 |   | 6")
+        print("- | - | -       1 | 2 | 3")
+        print("- | X | -       4 | 5 | 6")
         print("O | - | -       7 | 8 | 9")
         print("\n")
         print("\n")
@@ -71,25 +185,25 @@ while True:
         print("\n")  
     elif pole == 8:
         print("\n")
-        print("- | - | -       1 |   | 3")
-        print("- | X | -       4 |   | 6")
+        print("- | - | -       1 | 2 | 3")
+        print("- | X | -       4 | 5 | 6")
         print("- | O | -       7 | 8 | 9")
         print("\n")
         print("\n")
-        print("- | - | X       1 |   | 3")
-        print("- | X | -       4 |   | 6")
+        print("- | - | X       1 | 2 | 3")
+        print("- | X | -       4 | 5 | 6")
         print("- | O | -       7 | 8 | 9")
         print("\n")
 
     elif pole == 9:
         print("\n")
-        print("- | - | -       1 |   | 3")
-        print("- | X | -       4 |   | 6")
+        print("- | - | -       1 | 2 | 3")
+        print("- | X | -       4 | 5 | 6")
         print("- | - | 0       7 | 8 | 9")
         print("\n")
         print("\n")
-        print("- | - | X       1 |   | 3")
-        print("- | X | -       4 |   | 6")
+        print("- | - | X       1 | 2 | 3")
+        print("- | X | -       4 | 5 | 6")
         print("- | - | 0       7 | 8 | 9")
         print("\n")
     else:
